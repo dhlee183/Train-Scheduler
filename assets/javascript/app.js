0afd4b78fd135eff2cc445d3397c456ad0199b6f
David Hyun Lee <David Hyun Lee>
2016-09-20 22:40:25 -0400

    Initial files created

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAl9EFNpn314Fh1PJytmpc5raPzvlksYdI",
+	authDomain: "train-scheduler-42a88.firebaseapp.com",
+	databaseURL: "https://train-scheduler-42a88.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "44203733054"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
