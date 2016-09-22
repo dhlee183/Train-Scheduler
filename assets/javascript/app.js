3a596bbbacd61100e867102e23f1605515975a10
David Hyun Lee <David Hyun Lee>
2016-09-21 22:51:58 -0400

    Created initial logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,30 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+$('#submitButton').on('click', function() {
+	var trName = $('#trainName').val().trim();
+	var dest = $('#destination').val().trim();
+	var trTime = $('#trainTime').val().trim();
+	var freq = $('#frequency').val().trim();
+
+	database.ref().push({
+		TrainName: trName,
+		Destination: dest,
+		TrainTime: trTime,
+		Frequency: freq
+	});
+
+	return false;
+});
+
+database.ref().on('child_added', function(snapshot) {
+	var info = snapshot.val();
+	console.log(info);
+
+	$('#stats').prepend('<tr><td>' + info.TrainName + '</td><td>' 
+	+ info.Destination + '</td><td>' 
+	+ info.Frequency + '</td></tr>');
+	
+	}, function (errorObject) {
+		console.log('The read failed: ' + errorObject.code);
+});
