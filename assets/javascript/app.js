d70404c45a67ddcb015c97a0860d86dab8aa33d9
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2016-09-23 22:07:31 -0400

    Updated js file to create functioning application

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,20 +19,39 @@ $('#submitButton').on('click', function() {
 	database.ref().push({
 		TrainName: trName,
 		Destination: dest,
-		TrainTime: trTime,
-		Frequency: freq
-	});
+		Frequency: freq,
+		FirstTrainTime: trTime,
+	});	
+
+	$('#trainName').val("")
+	$('#destination').val("");
+	$('#trainTime').val("");
+	$('#frequency').val("");
 
 	return false;
 });
 
 database.ref().on('child_added', function(snapshot) {
 	var info = snapshot.val();
-	console.log(info);
+
+	var trainTime = info.FirstTrainTime
+	var tFreq = info.Frequency
+
+	var trainTimeConverted = moment(trainTime, 'hh:mm').subtract(1, 'years');
+
+	var diffTime = moment().diff(moment(trainTimeConverted), 'minutes');
+
+	var tRemain = diffTime % tFreq;
+
+	var tMinutesTillTrain = tFreq - tRemain;
+
+	var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+	var arrTime = moment(nextTrain).format('hh:mm A');
 
 	$('#stats').prepend('<tr><td>' + info.TrainName + '</td><td>' 
 	+ info.Destination + '</td><td>' 
-	+ info.Frequency + '</td></tr>');
+	+ info.Frequency + '</td><td>' + arrTime + '</td><td>' 
+	+ tMinutesTillTrain + '</td></tr>');
 	
 	}, function (errorObject) {
 		console.log('The read failed: ' + errorObject.code);
